7bf428b812460af2bba130d08fd0946fea2a3414
Fabien <fabien.ponzio@laplateforme.io>
2021-01-11 18:29:42 +0100

    Adding check login
    $checklogin permet de verifier si l'utilisateur est déja inscrit

--- config/functionT.php
+++ config/functionT.php
@@ -8,19 +8,29 @@ class User{
 //inscrition
 public function register($login,$password){
     secure($login);
+
     $password = password_hash($password, PASSWORD_DEFAULT);  
     $pdo = new PDO('mysql:host=localhost;dbname=reservationsalles','root',''); 
-    $stmt = $pdo->prepare("INSERT INTO utilisateurs (login,password) VALUE (?,?)");
-    $stmt->bindValue(1,$login);
-    $stmt->bindValue(2,$password);
-    $stmt->execute();
-    $id = $pdo->lastInsertId();
-    $this->id = $id;
-    $this->login = $login;
-    $this->password = $password;
-    header('Location:connexion.php');
-    return array($login,$password);
-} 
+    $checklogin = $pdo->prepare("SELECT login FROM utilisateurs WHERE login = :login");
+    $checklogin->bindValue(':login', $login);
+    $checklogin->execute(); 
+    $count= $checklogin->fetch();
+    if (!$count) {
+        $stmt = $pdo->prepare("INSERT INTO utilisateurs (login,password) VALUE (?,?)");
+        $stmt->bindValue(1,$login);
+        $stmt->bindValue(2,$password);
+        $stmt->execute();
+        $id = $pdo->lastInsertId();
+        $this->id = $id;
+        $this->login = $login;
+        $this->password = $password;
+        header('Location:connexion.php');
+        return array($login,$password);
+    }
+    else {
+        echo"L'identifiant existe déjà!";
+    }
+}
 //Update
 public function update($login,$password){
     $pdo = new PDO('mysql:host=localhost;dbname=reservationsalles','root','');
