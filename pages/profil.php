63094a193ea4d75316fdfe96cd7479133a7ca136
thomas-dellacase <thomas.dellacase@laplatefrome.io>
2021-01-13 17:38:23 +0100

    YEEEEEEEEEEEEEEEEEEES

--- pages/profil.php
+++ pages/profil.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-var_dump($_SESSION);
+//var_dump($_SESSION);
 /*include '../config/functionT.php';
 $newuser = new User();
 $db = new PDO('mysql:host=localhost;dbname=reservationsalles','root','');
@@ -9,34 +9,41 @@ if(isset($_POST['submit-newlog']) OR isset($_POST['submit-newpw'])){
 }*/
 
     try{
-        $db = new PDO ('mysql:host=localhost;dbname=reservationsalles','root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $db = new PDO ('mysql:host=localhost;dbname=reservationsalles','root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); echo 'coucou1';
     }
     catch (Exeception $e){
         die('Erreur : ' . $e->getMessage());
     }
-    if ( isset($_SESSION['login']) AND isset($_SESSION['id'])){    
+     var_dump($_SESSION);
+     $login = $_SESSION['utilisateur'];
+     $id =$_SESSION['id'];
+    /*if ( isset($_SESSION['login']) AND isset($_SESSION['id'])){    
         $login=$_SESSION['login'];
-        $id=$_SESSION['id'];
+        $id=$_SESSION['id'];*/ 
+       
 
-     if(isset($_POST['submit']) AND $_POST['newpassword'] == $_POST['confpassword']){
-         $error = 'Mot de passe et confirmation mot de passe différents';
+
+     if(isset($_POST['submit']) AND $_POST['newpassword'] != $_POST['confpassword']){
+         $error = 'Mot de passe et confirmation mot de passe différents';echo 'coucou4';
      }
      else{
-         if(isset($_POST['submit'])){
+         if(isset($_POST['submit'])){ echo 'coucou5';
              $newlogin=htmlspecialchars($_POST['newlogin']);
              $password = password_hash($_POST['newpassword'], PASSWORD_DEFAULT);
              $update= $db ->prepare("UPDATE utilisateurs SET login = :newlogin, password = :newpassword WHERE id = :id");
+             var_dump($update);
              $update ->execute(array(
                 'newlogin' => $newlogin,
                 'newpassword' => $password,
                 'id' => $id,
              ));
+             var_dump($update);
              $ok = 'Profil modifié';
-             $_SESSION['login'] =$newlogin;
+             $_SESSION['utilisateur'] =$newlogin;
              $login = $newlogin;
             } 
          }
-     }  
+      
 
    
      if (isset($ok)) {
@@ -48,7 +55,7 @@ if(isset($_POST['submit-newlog']) OR isset($_POST['submit-newpw'])){
 
 <form action="profil.php" method="POST" id="form-pro">
                             <label for="newlogin"></label>
-                            <input type="text"  name="newlogin" placeholder="Login" value=<?php if(isset($newuser->login)){echo $newuser->login ;} else {echo "";} ?> > <br>
+                            <input type="text"  name="newlogin" placeholder="Login"> <br>
                             <label for="oldpassword"></label>
                             <input type="password" name="newpassword" placeholder="New password"><br>
                             <label for="newpassword"></label>
