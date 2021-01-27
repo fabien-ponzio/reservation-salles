cca91e8ce50cea7e0971fc03d8e9db1f5824d15b
Fabien <fabien.ponzio@laplateforme.io>
2021-01-27 15:02:36 +0100

    janvoi

--- config/fonctions.php
+++ config/fonctions.php
@@ -1,6 +1,7 @@
 <?php
 //session_start();
-require('DB.php');
+// require('DB.php');
+require('User.php');
 
 
 class User{
--- reservationsallees.sql
+++ reservationsallees.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 27 jan. 2021 à 09:38
--- Version du serveur :  10.4.13-MariaDB
+-- Généré le : ven. 08 jan. 2021 à 09:12
+-- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -30,13 +30,13 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `reservations`;
 CREATE TABLE IF NOT EXISTS `reservations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titre` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `description` text COLLATE utf8mb4_bin NOT NULL,
+  `titre` varchar(255) COLLATE utf8_bin NOT NULL,
+  `description` text COLLATE utf8_bin NOT NULL,
   `debut` datetime NOT NULL,
   `fin` datetime NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -47,22 +47,10 @@ CREATE TABLE IF NOT EXISTS `reservations` (
 DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `login` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
---
--- Déchargement des données de la table `utilisateurs`
---
-
-INSERT INTO `utilisateurs` (`id`, `login`, `password`) VALUES
-(1, 'Thejeanjean', '$2y$10$qNwSvItE1v/POW6oTRxFnedo4p43ArHskQdihoaCuMVqeQKdwA92W'),
-(2, 'jeanfifou', '$2y$10$RCzkzfjzZaCakCHE2BLkX.MuRaOqXZXCaMHLatN33ZTeOz/zmWFMq'),
-(3, 'jeangandalf', '$2y$10$xURxiN1O0VtPleElaXrVkuzPBYSTjmxJ66vBflRaF.V9w1D.g84.G'),
-(4, 'FifouOne', '$2y$10$S/r1xNrrmoGEYBExwMUbAe7J36ilm8J0jcyoCT7VqRcJt5kawTG8.'),
-(5, 'fifouOne', '$2y$10$ZK0ck1v7EtcAKJVHv3JCUOJcRdH1e9ul2T4GQAkn996DFU9wPCxIq'),
-(6, 'TheFifou', '$2y$10$NlTw9b7c6Bl9ecRPJNK2S.G24BRKMvrsRffPtVkJaUNz3zNCJrf8O');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
